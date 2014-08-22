857f5cb85e2dadc12a6c8959b61bbf3dc6ac01cf
Jeramia Ory <jeramia.ory@gmail.com>
2014-08-21 22:08:18 -0400

    sketching out subproc

--- qual_trim.py
+++ qual_trim.py
@@ -6,3 +6,19 @@ import subprocess
 import glob
 
 def trim_single(filebase, file1):
+	trim_directory = filebase+'_trim'
+	trim_command = (""
+		""
+		""
+		""
+		""
+		) % (filebase, os.path.join("..",file1))
+		if not os.path.exists(trim_directory):
+		os.mkdir(trim_directory)
+	os.chdir(trim_directory)
+	if subprocess.call(trim_command, shell=True):
+		os.chdir("..")
+		return True
+	else:
+		os.chdir("..'")
+		return False
