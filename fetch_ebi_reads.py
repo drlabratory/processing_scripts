a4109c60f37380111b321f2c8fa5ed19b1b714bb
Jeramia Ory <jeramia.ory@gmail.com>
2014-11-11 10:55:06 -0500

    incorporating command line arguments

--- fetch_ebi_reads.py
+++ fetch_ebi_reads.py
@@ -1,4 +1,13 @@
-import ftplib
+import ftplib, sys, getopt
+
+
+def main(argv):                         
+    try:                                
+        opts, args = getopt.getopt(argv, "hl", ["help", "list"])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+
 
 ftp = ftplib.FTP("ftp.sra.ebi.ac.uk")
 ftp.login("anonymous","jeramiaory@kings.edu")
@@ -7,4 +16,7 @@ ftp.cwd("/vol1/fastq/SRR576/SRR576301")
 
 for files in ftp.nlst():
 	ftp.retrbinary('RETR %s' % files, open(files, 'wb').write)
-	
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
