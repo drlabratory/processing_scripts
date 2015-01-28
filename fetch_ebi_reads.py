591d80846fb1f8e2cd2fd25fe7ceb0b1b14ec1c8
Jeramia Ory <jeramia.ory@gmail.com>
2015-01-28 09:16:48 -0500

    Switching to argparse, seems more straightforward

--- fetch_ebi_reads.py
+++ fetch_ebi_reads.py
@@ -1,22 +1,26 @@
-import ftplib, sys, getopt
+import ftplib, argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+    	description='A script that downloads all fastq files from an SRR run',
+    	epilog='Highly specific to EBI ENA')
+    # parser.add_argument('-h', '--help')
+    parser.add_argument('-f', '--file', required=True,
+    	help='a file containing ENA read accesion strings, one per line')
+    args = parser.parse_args()
+
+print args
+
+# ftp = ftplib.FTP("ftp.sra.ebi.ac.uk")
+# ftp.login("anonymous","jeramiaory@kings.edu")
+
+# file_list = open(args.file, 'rb')
+# for run_name in file_list:
+# 	print name
 
 
-def main(argv):                         
-    try:                                
-        opts, args = getopt.getopt(argv, "hl", ["help", "list"])
-    except getopt.GetoptError:
-        usage()
-        sys.exit(2)
+# ftp.cwd("/vol1/fastq/SRR576/SRR576301")
 
 
-ftp = ftplib.FTP("ftp.sra.ebi.ac.uk")
-ftp.login("anonymous","jeramiaory@kings.edu")
-ftp.cwd("/vol1/fastq/SRR576/SRR576301")
-
-
-for files in ftp.nlst():
-	ftp.retrbinary('RETR %s' % files, open(files, 'wb').write)
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
+# for files in ftp.nlst():
+# 	ftp.retrbinary('RETR %s' % files, open(files, 'wb').write)
