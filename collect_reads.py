1f44bebd3dbb11528300a6e68d25eee23168b956
Jeramia Ory <jeramia.ory@gmail.com>
2015-04-16 10:53:18 -0400

    choosing mv vs cat and comenting in more delete statements

--- collect_reads.py
+++ collect_reads.py
@@ -1,40 +1,62 @@
 #!/usr/bin/env python
 
-import os
+import os, shutil
 import subprocess
 import glob
 
 # This script is run after qual_trim.py, and goes into each *_trim
-# directory and runs either intervleave-reads.py or cat
-# them combines them into a single file
+# directory and renames files, then cats / moves
+# them them into a single file
 
 trim_directories = glob.glob("SRR*_trim")
 
 for dir in trim_directories:
     filebase = dir.split('_')[0]
-    new_PE = filebase+".PE.qc.fq"
+    # new_PE = filebase+".PE.qc.fq"
+    new_PE1 = filebase+".PE.qc.1.fq"
+    new_PE2 = filebase+".PE.qc.2.fq"
     new_SE = filebase+".SE.qc.fq"
-    PE_command = "interleave-reads.py *P > %s" % new_PE
+    # PE_command = "interleave-reads.py *P > %s" % new_PE
+    PE1_command = "mv *1P %s" % new_PE1
+    PE2_command = "mv *2P %s" % new_PE2
     SE_command = "cat *U > %s" % new_SE
     os.chdir(dir)
-    subprocess.call(PE_command, shell=True)
+    # subprocess.call(PE_command, shell=True)
+    subprocess.call(PE1_command, shell=True)
+    subprocess.call(PE2_command, shell=True)
     subprocess.call(SE_command, shell=True)
     os.chdir("..")
 
-#now combine all the trimmed reads into one file for kmer analysis
+# now combine all the trimmed reads into one file
+# checks to see if only one set of reads and moves instead of cats if so
 
 project_name = os.getcwd().split('/')[-1].split('_')[-1]
-PE_join = "cat *_trim/*.PE.qc.fq > %s" % project_name+"_reads.PE.qc.fq"
-SE_join = "cat *_trim/*.SE.qc.fq > %s" % project_name+"_reads.SE.qc.fq"
+if len(glob.glob('*_trim/*.PE.qc.1.fq')) > 1:
+    PE1_join = "cat *_trim/*.PE.qc.1.fq > %s" % project_name+"_reads.PE.qc.1.fq"
+    PE2_join = "cat *_trim/*.PE.qc.2.fq > %s" % project_name+"_reads.PE.qc.2.fq"
+    SE_join = "cat *_trim/*.SE.qc.fq > %s" % project_name+"_reads.SE.qc.fq"
+else:
+    PE1_join = "mv -i *_trim/*.PE.qc.1.fq %s" % project_name+"_reads.PE.qc.1.fq"
+    PE2_join = "mv -i *_trim/*.PE.qc.2.fq %s" % project_name+"_reads.PE.qc.2.fq"
+    SE_join = "mv -i *_trim/*.SE.qc.fq %s" % project_name+"_reads.SE.qc.fq"
 
-subprocess.call(PE_join, shell=True)
+
+# subprocess.call(PE_join, shell=True)
+subprocess.call(PE1_join, shell=True)
+subprocess.call(PE2_join, shell=True)
 subprocess.call(SE_join, shell=True)
 
-# get rid of the original read files (now pulling the trigger on this yet)
+# Compress all fq files
+subprocess.call("pigz --best *.fq")
+
+# get rid of the original read files (not pulling the trigger on this yet)
 # also should delete *_trim directories
 
-del_files = glob.glob("SRR*gz")
+# del_files = glob.glob("SRR*gz")
+# del_directories = glob.glob("*_trim")
 
-for files in del_files:
-    print files
+# for files in del_files:
 #     os.remove(files)
+# for directories in del_directories:
+#     shutil.rmtree(directories)
+
